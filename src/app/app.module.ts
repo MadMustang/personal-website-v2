d4939ba2541da1e0646fa11b0641e71edd08c846
Rionaldy "Roy" Triantoro <triantorowoozie@gmail.com>
2020-06-21 00:02:07 +0700

    Created draft for intro section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './components/layout/nav-menu/nav-menu.component';
+import { IntroComponent } from './components/content/intro/intro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent
+    NavMenuComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule
